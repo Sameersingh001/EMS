054bbbbd7623ed91d39b70fb0c0a68f73e6121f0
Sameer singh <mrsameersingh001@gmail.com>
2025-08-20 18:54:23 +0530

    connection changed

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/EmployeesDB")
+    await mongoose.connect("mongodb+srv://mrsameersingh001:kZUvbFlcBXXWwOXF@employeedb.djxmajg.mongodb.net/")
     .then(() => console.log("Successfully"))
     .catch((err) => console.log("connection error",err))
-}
+}   
 
 export default connectDB
